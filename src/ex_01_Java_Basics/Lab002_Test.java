5ca78fc9782c5888e553f3d8384681db3e110010
palvailokesh-git <write2lokeshpalvai@gmail.com>
2025-12-15 20:31:00 +0530

    adding new class in ex_01_Lab002

---
+++ src/ex_01_Java_Basics/Lab002_Test.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Test {
+    public static void main(String[] args){
+        System.out.println("Test Check");
+        System.out.println("what are you doing");
+    }
+}
